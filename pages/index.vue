ddd179bfb25938d4e5ce8868e5790cc7cd85039e
Jonas Reif <reif.jonas@gmx.de>
2023-11-10 15:58:25 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>Cookie first site:</h1>
+        <ol>
+            <li>Set the cookie: <button @click="setCookie()">Set the cookie</button></li>
+            <li>Check cookies in Chrome Dev Tools</li>
+            <li>
+                Navigate to second page via NuxtLink: <NuxtLink to="/second">Navigate to second page via NuxtLink</NuxtLink>
+            </li>
+        </ol>
+    </div>
+  </template>
+  
+<script setup>
+  function setCookie() {
+    const dataExpiresAt = new Date(
+      new Date().getTime() + 365 * 24 * 60 * 60 * 1000
+    );
+  
+    useCookie('test', {
+      path: '/',
+      expires: dataExpiresAt,
+    }).value = true;
+  }
+  </script>
+  
---
+++ pages/second.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <h1>Cookie second site:</h1>
+    <p>We call useCookie('test') to fetch the value: {{ cookie }}</p>
+    <ul>
+        <li>Check cookies in Chrome Dev Tools</li>
+        <li>Navigate back using browser back and check cookie again</li>
+        <li>Navigate back using NuxtLink: <NuxtLink to="/">Navigate to first page via NuxtLink</NuxtLink></li>
+    </ul>
+</div>
+</template>
+<script setup>
+const cookie = useCookie('test').value;
+</script>
